3e93437e72ba7d571022223ee9fa17bb3f03876d
tanujkher <tanujkher15@gmail.com>
2020-01-31 16:25:44 +0530

    Add done not-done functionality line through

--- script.js
+++ script.js
@@ -4,8 +4,24 @@ $(() => {
     let ulTasks = $('#ulTasks')
     let inpNewTask = $('#inpNewTask')
 
+    // Add functionality
+
     btnAdd.click(() => {
-        ulTasks.append($('<li>').text(inpNewTask.val()).attr('class', 'list-group-item'))
+        // ulTasks.append($('<li>').text(inpNewTask.val()).attr('class', 'list-group-item'))
+        let listItem = $('<li>', {
+            'class' : 'list-group-item',
+            text : inpNewTask.val()
+        })
+        listItem.click(() => {
+            listItem.toggleClass('done')
+        })
+        ulTasks.append(listItem)
+        inpNewTask.val('')
+    })
+
+    // Clear functionality
+
+    btnClear.click(() => {
         inpNewTask.val('')
     })
 })
